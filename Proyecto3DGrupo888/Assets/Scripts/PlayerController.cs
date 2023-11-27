33827789f30ab899f2670cab265c5dd895be5983
celidearaceli <celidearaceli@gmail.com>
2023-11-27 10:56:00 -0300

    Cambios en el codigo de PlayerController

--- Proyecto3DGrupo888/Assets/Scripts/PlayerController.cs
+++ Proyecto3DGrupo888/Assets/Scripts/PlayerController.cs
@@ -2,73 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-
 public class PlayerController : MonoBehaviour
 {
-    public float walkingSpeed = 7.5f;
-    public float runningSpeed = 11.5f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-    public Camera playerCamera;
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 45.0f;
+    // Referencia al componente CharacterController del jugador.
+    private CharacterController player;  
+    // Velocidad de movimiento del jugador, editable desde el inspector.
+    [SerializeField] private float moveSpeed;  
+    // Referencia a la cámara, editable desde el inspector.
+    [SerializeField] Camera camara;  
+    // Vectores para almacenar la entrada del jugador y la dirección de movimiento.
+    private Vector3 axis, movePlayer;  
 
-    CharacterController characterController;
-    Vector3 moveDirection = Vector3.zero;
-    float rotationX = 0;
-
-    [HideInInspector]
-    public bool canMove = true;
-
-    void Start()
+    private void Awake()
     {
-        characterController = GetComponent<CharacterController>();
-
-        // Lock cursor
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        // Obtener el componente CharacterController del objeto al que se adjunta este script.
+        player = GetComponent<CharacterController>();  
     }
 
-    void Update()
+    private void Update()
     {
-        // We are grounded, so recalculate move direction based on axes
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        // Rotar el jugador horizontalmente basado en la entrada del ratón.
+        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
 
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        // Obtener la entrada del jugador para el movimiento en el eje horizontal y vertical.
+        axis = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        // Normalizar el vector de entrada si su magnitud es mayor que 1.
+        if (axis.magnitude > 1)
         {
-            moveDirection.y = jumpSpeed;
+            axis = transform.TransformDirection(axis).normalized;
         }
         else
         {
-            moveDirection.y = movementDirectionY;
+            axis = transform.TransformDirection(axis);
         }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        if (!characterController.isGrounded)
-        {
-            moveDirection.y -= gravity * Time.deltaTime;
-        }
-
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
-
-        // Player and Camera rotation
-        if (canMove)
-        {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-        }
+        // Mover al jugador en la dirección calculada.
+        player.Move(axis);
+        // player.Move(axis * moveSpeed * Time.deltaTime);
     }
 }
